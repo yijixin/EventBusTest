4ab3c23e126380d6278bfd366fbb743ed1686538
yijixin <1093546996@qq.com>
2017-03-30 17:33:50 +0800

    2017-03-30

--- app/src/main/java/vp/com/eventbustest/fragment/RightFragment.java
+++ app/src/main/java/vp/com/eventbustest/fragment/RightFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -14,6 +15,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import vp.com.eventbustest.R;
+import vp.com.eventbustest.bean.ItemList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +23,10 @@ import vp.com.eventbustest.R;
 public class RightFragment extends Fragment {
 
 
+    private Button btn;
     private TextView tvName;
+
+    private int i = 0;
 
     public RightFragment() {
         // Required empty public constructor
@@ -38,8 +43,21 @@ public class RightFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_right, container, false);
+        btn = ((Button) view.findViewById(R.id.btn));
         tvName = ((TextView) view.findViewById(R.id.tv_name));
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new ItemList(100+ i,"y"));
+                i++;
+            }
+        });
     }
 
     @Override
@@ -52,7 +70,15 @@ public class RightFragment extends Fragment {
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEventMainThread(String name) {
         if (name!=null){
-            tvName.setText(name);
+            btn.setText(name);
+        }
+    }
+
+    //本页面传值
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent_MainThread(ItemList itemList) {
+        if (itemList!=null){
+            tvName.setText(itemList.getId()+"-"+itemList.getName());
         }
     }
 
